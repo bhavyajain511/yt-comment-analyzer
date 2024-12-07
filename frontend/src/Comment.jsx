63e7d0c1c94d15e4500ab2d9dfd40b48abbfb655
bhavyajain511 <bjbhavyajain2004@gmail.com>
2024-12-07 18:52:41 +0530

    update comment structure

--- frontend/src/Comment.jsx
+++ frontend/src/Comment.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Divider } from '@mui/material';
 
 // Comment Component
-const Comment = ({ comment, likes , sentiment }) => {
+const Comment = ({ comment, likes , sentiment ,timestamp, rating}) => {
     return (
         <Card variant="outlined" style={{ marginBottom: '16px', width:'70%' }}>
             <CardContent>
@@ -19,6 +19,14 @@ const Comment = ({ comment, likes , sentiment }) => {
                 <Divider style={{ margin: '16px 0' }} />
                 <Typography variant="body2" color="text.secondary">
                     Sentiment: {sentiment}
+                </Typography>
+                <Divider style={{ margin: '16px 0' }} />
+                <Typography variant="body2" color="text.secondary">
+                    Timestamp: {new Date(timestamp).toLocaleString('en-US', { timeZone: 'UTC', timeZoneName: 'short' })}
+                </Typography>
+                <Divider style={{ margin: '16px 0' }} />
+                <Typography variant="body2" color="text.secondary">
+                    Rating: {rating}
                 </Typography>
             </CardContent>
         </Card>
