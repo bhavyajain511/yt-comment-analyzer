c39ae574d581a653aad4f87194bb65340aadc195
bhavyajain511 <bjbhavyajain2004@gmail.com>
2024-12-08 19:47:25 +0530

    Clean up unused code

--- python-backend/comment_analyzer.py
+++ python-backend/comment_analyzer.py
@@ -1,10 +1,8 @@
-from textblob import TextBlob
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 from model import predict_sentiment_f 
 
-# main
 def analyze_comments(comments):
     """
     Function to analyze the sentiment of comments using predict_sentiment_f.
@@ -32,55 +30,3 @@ def analyze_comments(comments):
 
     return analyzed_comments
 
-# def analyze_comments(comments):
-#     """
-#     Function to analyze the sentiment of comments using predict_sentiment_f.
-#     """
-#     analyzed_comments = []
-
-#     for comment in comments:
-#         comment_text = comment["comment"]
-#         # Classify sentiment using predict_sentiment_f
-#         sentiment = predict_sentiment_f(comment_text)
-
-#         # Add sentiment label to the comment
-#         analyzed_comment = {
-#             "comment": comment_text,
-#             "num_of_likes": comment["num_of_likes"],
-#             "sentiment": sentiment
-#         }
-
-#         analyzed_comments.append(analyzed_comment)
-
-#     return analyzed_comments
-
-# def analyze_comments(comments):
-#     """
-#     Function to analyze the sentiment of comments using TextBlob.
-#     """
-#     analyzed_comments = []
-
-#     for comment in comments:
-#         comment_text = comment["comment"]
-#         # Analyze sentiment using TextBlob
-#         analysis = TextBlob(comment_text)
-#         # Classify sentiment based on polarity
-#         if analysis.sentiment.polarity > 0.1:
-#             sentiment = "positive"
-#         elif analysis.sentiment.polarity < -0.1:
-#             sentiment = "negative"
-#         else:
-#             sentiment = "neutral"
-
-#         # Add sentiment label to the comment
-#         analyzed_comment = {
-#             "comment": comment_text,
-#             "num_of_likes": comment["num_of_likes"],
-#             "sentiment": sentiment,
-#              "timestamp": comment["timestamp"],
-#             "rating":analysis.sentiment.polarity
-#         }
-
-#         analyzed_comments.append(analyzed_comment)
-
-#     return analyzed_comments
