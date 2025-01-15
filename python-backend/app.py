de318862a7825cd5c175468c570432f09b1789a2
bhavyajain511 <bjbhavyajain2004@gmail.com>
2025-01-16 00:48:54 +0530

    make changes for deployement

--- python-backend/app.py
+++ python-backend/app.py
@@ -117,4 +117,4 @@ def analyze():
         return jsonify({"error": "Invalid YouTube link provided."}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
